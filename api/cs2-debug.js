9f7a44a1321eef3f494b798668465283b7c281cd
cryptobatmanEV <tybatman94@gmail.com>
2026-05-26 12:19:25 -0500

    Update cs2-debug.js

--- api/cs2-debug.js
+++ api/cs2-debug.js
@@ -8,43 +8,51 @@ async function q(query) {
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  const out = {};
 
-  // 1. Introspect ALL types fully
-  const schema = await q(`{ __schema { types {
-    name
-    fields { name type { name kind ofType { name kind ofType { name } } } }
-  } } }`);
-  const types = schema?.data?.__schema?.types || [];
-  
-  // Show SeriesPlayerState and nearby types
-  for (const t of types) {
-    if (!t.name || t.name.startsWith('__')) continue;
-    if (t.fields?.length) {
-      out['type_' + t.name] = t.fields.map(f => 
-        `${f.name}: ${f.type?.name || f.type?.ofType?.name || f.type?.kind}`
-      );
-    }
-  }
-
-  // 2. Try seriesState without nickname - just id + stats fields
-  const r2 = await q(`{
-    seriesState(id:"2913007") {
+  // Query Techno's series 2913007 (March 13 vs NaVi) with correct CS2 fields
+  const r = await q(`{
+    seriesState(id: "2913007") {
       id
+      startedAt
+      finished
       teams {
         id
-        players { id kills deaths assists headshots won }
+        name
+        won
+        score
+        players {
+          id
+          name
+          kills
+          deaths
+          killAssistsGiven
+          ... on SeriesPlayerStateCs2 { headshots }
+          ... on SeriesPlayerStateCsgo { headshots }
+        }
       }
       games {
-        id sequenceNumber
+        id
+        sequenceNumber
+        started
+        finished
+        map { name }
         teams {
           id
-          players { id kills deaths assists headshots }
+          name
+          won
+          players {
+            id
+            name
+            kills
+            deaths
+            killAssistsGiven
+            ... on GamePlayerStateCs2 { headshots }
+            ... on GamePlayerStateCsgo { headshots }
+          }
         }
       }
     }
   }`);
-  out.seriesState = r2?.data || r2?.errors?.map(e=>e.message);
 
-  return res.json(out);
+  return res.json(r?.data || r?.errors);
 }
