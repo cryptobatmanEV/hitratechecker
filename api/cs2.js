3ca79e00128ff09f92e1c0d998f144fbdd7a1b08
cryptobatmanEV <tybatman94@gmail.com>
2026-05-27 20:57:56 -0500

    Update cs2.js

--- api/cs2.js
+++ api/cs2.js
@@ -275,6 +275,7 @@ async function getGridOnlyGames(teamId, slug) {
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin','*');
   res.setHeader('Access-Control-Allow-Methods','GET,OPTIONS');
+  res.setHeader('Cache-Control','no-store');
   if(req.method==='OPTIONS') return res.status(200).end();
   const {action,playerId,stat}=req.query;
   const nickname=req.query.nickname||req.query.q||'';
